7f1bb6c58dec62a34053cbea98dc31b03e70ee2f
Jakub Cabala <jcabala@jcabala>
2023-07-07 16:34:25 +0200

    Authentication testing

---
+++ test/Auth.t.sol
@@ -0,0 +1,33 @@
+pragma solidity ^0.8.18;
+
+import "forge-std/Test.sol";
+import {Wallet} from "../src/Wallet.sol";
+
+contract AuthTest is Test {
+    Wallet public wallet;
+
+    function setUp() public {
+        wallet = new Wallet();
+    }
+
+    function testSetOwner() public {
+        wallet.setOwner(address(1));
+        assertEq(wallet.owner(), address(1));
+    }
+
+    function testFailNotOwner() public {
+        vm.prank(address(1));
+        wallet.setOwner(address(1));
+    }
+
+    function testFailSetOwnerAgain() public {
+        wallet.setOwner(address(1));
+
+        vm.startPrank(address(1));
+        wallet.setOwner(address(1));
+        wallet.setOwner(address(1));
+        vm.stopPrank();
+
+        wallet.setOwner(address(1));
+    }
+}
