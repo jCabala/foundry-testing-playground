fbbab34c7182f54e7750de68f3e5ccad38e40000
Jakub Cabala <jcabala@jcabala>
2023-07-07 17:07:11 +0200

    Testing for reverting and custom errors

---
+++ test/Error.t.sol
@@ -0,0 +1,31 @@
+pragma solidity ^0.8.18;
+
+import "forge-std/Test.sol";
+import {Error} from "../src/Error.sol";
+
+contract ErrorTest is Test {
+    Error public err;
+
+    function setUp() public {
+        err = new Error();
+    }
+
+    function testFail() public {
+        err.throwError();
+    }
+
+    function testRevrert() public {
+        vm.expectRevert();
+        err.throwError();
+    }
+
+    function testRequireMessage() public {
+        vm.expectRevert(bytes("not authorized"));
+        err.throwError();
+    }
+
+    function testThrowCustomError() public {
+        vm.expectRevert(Error.NotAuthorized.selector);
+        err.throwCustomError();
+    }
+}
